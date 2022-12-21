02787e85775d2191904e52c05116c197890d5880
DiegoRodas20 <diegorodas2008@hotmail.com>
2022-12-21 21:49:04 +0100

    Startup configure

--- Scharff.API/Program.cs
+++ Scharff.API/Program.cs
@@ -1,36 +1,17 @@
-﻿using MediatR;
-using Npgsql;
-using Scharff.Infrastructure.Queries.Client.GetAllClients;
-using Scharff.Infrastructure.Queries.Client.GetClientById;
-using Scharff.Infrastructure.Repositories.Client.RegisterClient;
-using System.Data;
-using System.Reflection;
-
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddTransient<IDbConnection>(x => new NpgsqlConnection(builder.Configuration.GetConnectionString("Scharff_BD")));
-
-builder.Services.AddTransient(typeof(IGetClientById), typeof(GetClientById));
-builder.Services.AddTransient(typeof(IRegisterClientRepository), typeof(RegisterClientRepository));
-builder.Services.AddTransient(typeof(IGetAllClients), typeof(GetAllClients));
-
-Assembly application = AppDomain.CurrentDomain.Load("Scharff.Application");
-builder.Services.AddMediatR(application);
-
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
+namespace Scharff.API
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseAuthorization();
-app.MapControllers();
-
-app.Run();
-
