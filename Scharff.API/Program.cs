bb3b23f32c3308455288a5065bbbcfc4710cc402
DiegoRodas20 <diegorodas2008@hotmail.com>
2022-12-21 21:58:05 +0100

    cors

--- Scharff.API/Program.cs
+++ Scharff.API/Program.cs
@@ -1,17 +1,46 @@
-namespace Scharff.API
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+﻿using MediatR;
+using Npgsql;
+using Scharff.Infrastructure.Queries.Client.GetAllClients;
+using Scharff.Infrastructure.Queries.Client.GetClientById;
+using Scharff.Infrastructure.Repositories.Client.RegisterClient;
+using System.Data;
+using System.Reflection;
 
-        private static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddTransient<IDbConnection>(x => new NpgsqlConnection(builder.Configuration.GetConnectionString("Scharff_BD")));
+
+builder.Services.AddCors(options =>
+    {
+        options.AddPolicy("AllowAll", p =>
+        {
+            p.AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+        });
+    });
+
+builder.Services.AddTransient(typeof(IGetClientById), typeof(GetClientById));
+builder.Services.AddTransient(typeof(IRegisterClientRepository), typeof(RegisterClientRepository));
+builder.Services.AddTransient(typeof(IGetAllClients), typeof(GetAllClients));
+
+Assembly application = AppDomain.CurrentDomain.Load("Scharff.Application");
+builder.Services.AddMediatR(application);
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
+
