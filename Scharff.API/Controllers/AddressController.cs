ccaf2ed453ff2590d2b6a4d28b0a5ec59f8a5b5d
DiegoRodas20 <diegorodas2008@hotmail.com>
2022-12-29 21:37:30 +0100

    .

--- Scharff.API/Controllers/AddressController.cs
+++ Scharff.API/Controllers/AddressController.cs
@@ -35,9 +35,6 @@ namespace Scharff.API.Controllers
             return Ok(new CustomResponse<List<DirectionModel>>($"Se encontraron las direcciones con el id cliente: {idClient}.", result));
         }
 
-       
-
-
         [HttpPost]        
         [SwaggerOperation("Registrar direccion")]
         public async Task<IActionResult> RegisterDirection([FromBody] RegisterAddressCommand request)
